4619f18ce48e3d315c6761ceb01534058be260b2
cylowen <chris.vogel@mailbox.org>
2024-02-08 15:27:47 +0100

    minor changes

--- Scenes/Background/Background.gd
+++ Scenes/Background/Background.gd
@@ -1,10 +1,6 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func set_button_text(text: String, button_name:String) -> void:
 	if button_name == "orange_button":
 		$Timer/Label2.text = text
@@ -26,11 +22,6 @@ func set_button_clickability(clickability: bool, button_name:String) -> void:
 			$JoinGame.mouse_filter = Control.MOUSE_FILTER_IGNORE
 		else:
 			$JoinGame.mouse_filter = Control.MOUSE_FILTER_PASS
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_HostGame_pressed():
 	SignalManager.emit_signal("on_blue_button_pressed")
