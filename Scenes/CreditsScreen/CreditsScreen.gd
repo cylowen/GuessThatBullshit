8bee95bc474144422b09ae5313931e18c21d5202
Jonathan Derrough <jonathan.derrough@gmail.com>
2024-02-12 14:13:30 +0100

    Puts the labels and texture from the credits in *BoxContainers

--- Scenes/CreditsScreen/CreditsScreen.gd
+++ Scenes/CreditsScreen/CreditsScreen.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_CreditsScreen_visibility_changed():
-	$Label2.text = str(ScoreManager.get_score())
+	$VBoxContainer/HBoxContainer/Label2.text = str(ScoreManager.get_score())
